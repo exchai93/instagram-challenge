97c826e0d0f23eb30a390426b9cb66197405c5d1
Emily Chai <echai93@gmail.com>
2017-05-06 15:41:26 +0100

    WIP feature test for photo

--- spec/features/photos_features_spec.rb
+++ spec/features/photos_features_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
-feature 'photos' do
-  context 'no photo have been posted' do
+feature 'Photos' do
+  context 'no photos have been posted' do
     scenario 'should display a prompt to post a photo' do
       visit '/photos'
       expect(page).to have_content 'No photos yet'
       expect(page).to have_link 'Post a restaurant'
+    end
+  end
+
+  context 'photos have been posted' do
+    before do
+      Photo.create(caption: 'My first photo')
+    end
+
+    scenario 'display photos' do
+      visit '/photos'
+      expect(page).to have_content('My first photo')
+      expect(page).not_to have_content('No photos yet')
     end
   end
 end
