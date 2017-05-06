d6c84859b2fafc15fb957d7757465a48bbefc1bc
Emily Chai <echai93@gmail.com>
2017-05-06 16:01:14 +0100

    WIP test for posting a new photo

--- spec/features/photos_features_spec.rb
+++ spec/features/photos_features_spec.rb
@@ -13,11 +13,24 @@ feature 'Photos' do
     before do
       Photo.create(caption: 'My first photo')
     end
-
     scenario 'display photos' do
       visit '/photos'
       expect(page).to have_content('My first photo')
       expect(page).not_to have_content('No photos yet')
     end
   end
+
+  context 'posting photos' do
+    scenario 'prompts user to choose a photo from their library, then displays the new photo' do
+      visit '/photos'
+      click_link 'Post a photo'
+      fill_in 'Caption', with 'My first photo'
+      click_button 'Post'
+      expect(page).to have_content 'My first photo'
+      expect(current_path).to eq '/photos'
+
+    end
+  end
+
+
 end
